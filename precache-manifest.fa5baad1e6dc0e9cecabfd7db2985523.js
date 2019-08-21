8e81c8f173166b9aa55f1f2b899ed1ee9241d8bf
Janet <janetwang101@gmail.com>
2019-08-20 19:29:59 -0700

    Updates

---
+++ precache-manifest.fa5baad1e6dc0e9cecabfd7db2985523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1af6a93d93faec890f1d32fae06e024",
+    "url": "/design_system/index.html"
+  },
+  {
+    "revision": "3513942b50a9995b7037",
+    "url": "/design_system/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5d7e06f0082dd96bc43e",
+    "url": "/design_system/static/js/2.3398c4b1.chunk.js"
+  },
+  {
+    "revision": "3513942b50a9995b7037",
+    "url": "/design_system/static/js/main.7c3b2bf1.chunk.js"
+  },
+  {
+    "revision": "1385bb4c691fb270ab48",
+    "url": "/design_system/static/js/runtime~main.0d5df362.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/design_system/static/media/logo.5d5d9eef.svg"
+  }
+]);
