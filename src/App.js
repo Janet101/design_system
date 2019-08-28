184b1fa89b0854f977688caf7fb7ce98598908c7
Janet <janetwang101@gmail.com>
2019-08-28 01:15:21 -0700

    react JS - 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Icons,Tables, Button } from './components'
+import {TableBody,TableHead,TableCell,TableRow, Table,Paper} from "./components"
+
+
+function createData (name, calories, fat, carbs, protein) {
+    return {name, calories, fat, carbs, protein};
+}
+
+class App extends React.Component {
+
+    render() {
+        const rows = [
+            createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+            createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+            createData('Eclair', 262, 16.0, 24, 6.0),
+            createData('Cupcake', 305, 3.7, 67, 4.3),
+            createData('Gingerbread', 356, 16.0, 49, 3.9),
+        ];
+        const Icon = Icons.AccessAlarm;
+        return (
+            <div className="App">
+                <Icon/>
+                <Button variant="contained" color="primary">
+                    Primary
+                </Button>
+                <Paper >
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Dessert (100g serving)</TableCell>
+                                <TableCell align="right">Calories</TableCell>
+                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map(row => (
+                                <TableRow key={row.name}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right">{row.carbs}</TableCell>
+                                    <TableCell align="right">{row.protein}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Paper>
+
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import * as Icons from '@material-ui/icons/';
+import * as Charts from 'react-vis';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+// export all components, Icons, Charts
+export * from '@material-ui/core/';
+export { Icons, Charts};
+export {TableBody,TableHead,TableCell,TableRow,Table,Paper};
